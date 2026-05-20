c055b38b170a2a2dbc9c89c34a97fd291ec40f37
Marioo-Jr <mario.junior23@univille.br>
2026-05-20 19:29:27 -0300

    fix validation

--- ecommerce/src/main/java/com/ecommerceJava/ecommerce/dto/ProductDTO.java
+++ ecommerce/src/main/java/com/ecommerceJava/ecommerce/dto/ProductDTO.java
@@ -7,6 +7,7 @@ import com.ecommerceJava.ecommerce.entities.Category;
 import com.ecommerceJava.ecommerce.entities.Product;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -24,6 +25,7 @@ public class ProductDTO {
     @NotBlank(message = "Campo requerido.")
     private String description;
 
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo.")
     private Double price;
 
